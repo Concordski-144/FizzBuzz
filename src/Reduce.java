45393b3edb28bfb62e624681df89f438d27edc9a
Concordski-laptop <franklin.wu.1231@gmail.com>
2023-09-11 16:09:50 -0400

    initial commit

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int count = 0;
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i /= 2;
+            }
+            else {
+                --i;
+            }
+            ++count;
+        }
+        System.out.println(count);
+        // 50 25 24 12 6 3 2 1 0
+    }
+}
